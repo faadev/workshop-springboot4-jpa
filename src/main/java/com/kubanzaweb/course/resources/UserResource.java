e67243f6c7ad7a5fa3b9e7a9cbd126a7a34c8964
Faustino Alexandre <faustinoalfredo06@gmail.com>
2023-12-11 20:16:41 +0100

    User entity and resource

---
+++ src/main/java/com/kubanzaweb/course/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.kubanzaweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kubanzaweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		//Instanciar o Usuario
+		
+		User u = new User(1L, "Faustino", "faustino@gmail.com", "99999999", "123456");
+		
+		return ResponseEntity.ok().body(u);
+		
+	}
+    
+}
