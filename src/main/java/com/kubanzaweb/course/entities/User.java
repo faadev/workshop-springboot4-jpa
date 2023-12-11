352964bccdf4177a389246c79cf51428b9fdd28a
Faustino Alexandre <faustinoalfredo06@gmail.com>
2023-12-12 00:18:27 +0100

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/kubanzaweb/course/entities/User.java
+++ src/main/java/com/kubanzaweb/course/entities/User.java
@@ -1,20 +1,24 @@
 package com.kubanzaweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
+	
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -23,6 +27,8 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -36,6 +42,8 @@ public class User implements Serializable {
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+		
+		
 	}
 
 
@@ -87,7 +95,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -106,6 +117,8 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 	
 	
 
